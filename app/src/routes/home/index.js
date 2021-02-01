9501e5bc2ee8fee5f3d145e0c4f91d8d7c8a2283
wlwlsus <159632_@naver.com>
2021-02-01 19:10:08 +0900

    Add Front Login feature

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -6,9 +6,7 @@ const router = express.Router();
 const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.output.hello);
-
 router.get("/login", ctrl.output.login);
-
 router.post("/login", ctrl.process.login);
 
 module.exports = router;
