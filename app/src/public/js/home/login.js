2116a415d457ef0652db4ae0afdeaa23dedff7cc
wlwlsus <159632_@naver.com>
2021-02-01 17:23:05 +0900

    Add id, pass value check

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,15 @@
 "use strict";
 
-console.log("Hello..ㅋㅋㅋㅋ");
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    loginbtn = document.querySelector("button");
+
+loginbtn.addEventListener("click",login);
+
+function login(){
+    const req = {
+        id : id.value,
+        psword : psword.value,
+    };
+    console.log(req)
+}
