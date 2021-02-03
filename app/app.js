8de50b795bb0c4ce069a5dc18243607b619c51d1
wlwlsus <159632_@naver.com>
2021-02-04 02:16:54 +0900

    Add Register Route

--- app/app.js
+++ app/app.js
@@ -16,7 +16,7 @@ app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
 
 app.use(bodyParser.json());
-// URL을 통해 전달되는 데이터에 하글, 공백 등과 같은 문자가 포함될 경우 제대로 인식하지 않는 문제 해결
+// URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식하지 않는 문제 해결
 app.use(bodyParser.urlencoded({ extended: true}));
 
 // 하단에 위치 해야함
--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -11,17 +11,16 @@ const id = document.querySelector("#id"),
 registerBtn.addEventListener("click",register);
 
 function register(){
+    if(!id.value) return alert("아이디를 입력해주십시오.")
+    if(psword.value !== confirmPsword.value){
+        return alert("비밀번호가 일치하지 않습니다.")
+    }
+
     const req = {
         id : id.value,
         name : name.value,
         psword : psword.value,
-        confirmPsword : confirmPsword.value
     };
-
-    console.log(req);
-
-    // console.log(req);
-    // console.log(JSON.stringify(req));
 
     fetch("/register",{
         method: "POST",
@@ -38,6 +37,6 @@ function register(){
             alert(res.msg);
         }
     }).catch((err)=>{
-        console.error(new Error("회원가입 중 에러 발생"))
+        console.error(new Error("회원가입 중 에러 발생"));
     });
 }
